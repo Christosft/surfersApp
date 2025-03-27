60e5524b6789f97ac33544f77d062ceab0bace22
Chris Ftoulis <christosftoulis3@gmail.com>
2025-03-27 09:06:18 +0200

    feat: finish DBUtil

--- src/main/java/gr/surfapp/cf/util/DBUtil.java
+++ src/main/java/gr/surfapp/cf/util/DBUtil.java
@@ -1,6 +1,36 @@
 package gr.surfapp.cf.util;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class DBUtil {
 
+    private static final BasicDataSource ds = new BasicDataSource();
+    private static Connection connection;
 
+    static {
+        ds.setUrl("jdbc:mysql://localhost:3306/surf_Sessions_db?serverTimezone=UTC");
+        ds.setUsername("surfersSes");
+        ds.setPassword(System.getenv("PASSWD_SURFSES"));
+
+        ds.setInitialSize(10);
+        ds.setMinIdle(10);
+        ds.setMaxIdle(15);
+    }
+
+    private DBUtil() {
+
+    }
+
+    public static Connection getConnection() throws SQLException {
+        connection = ds.getConnection();
+
+        return connection;
+    }
+
+    public static void closeConnection() throws SQLException {
+        if (connection != null) connection.close();
+    }
 }
